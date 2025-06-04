e9bc9525c0ca8fb0c2814703ee114a7ccd86a78a
muuo <anthonymuuo92@gmail.com>
2025-06-04 16:30:20 +0300

    made the color on the model to shuffle between them and events created on first time

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { Fragment, useId, useMemo, useState } from "react";
 import {
   startOfWeek,
   startOfMonth,
@@ -14,7 +14,7 @@ import {
 } from "date-fns";
 import { formatDate } from "../utilis/formatDate";
 import { cc } from "../utilis/cc";
-import { useEvent } from "../context/useEvent";
+import { EVENT_COLORS, useEvent } from "../context/useEvent";
 import Modal, { type ModalProps } from "./Modal";
 import type { UnionType } from "../utilis/types";
 import type { Event } from "../context/Events";
@@ -146,75 +146,69 @@ function EventFormModal({
   event,
   ...modalProps
 }: EventFormModalProps) {
+  const isNew = event == null;
+  const formId = useId();
+  const [selectedColor, setSelectedColor] = useState(
+    event?.color || EVENT_COLORS[0]
+  );
   return (
     <Modal {...modalProps}>
       <div className="modal-title">
-        <div>Add Event</div>
-        <small>6/8/23</small>
-        <button className="close-btn">&times;</button>
+        <div>{isNew ? "Add" : "Edit"} Event</div>
+        <small>{formatDate(date || event.date, { dateStyle: "short" })}</small>
+        <button className="close-btn" onClick={modalProps.onClose}>
+          &times;
+        </button>
       </div>
       <form>
         <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" />
+          <label htmlFor={`${formId}-name`}>Name</label>
+          <input type="text" name="name" id={`${formId}-name`} />
         </div>
         <div className="form-group checkbox">
-          <input type="checkbox" name="all-day" id="all-day" />
-          <label htmlFor="all-day">All Day?</label>
+          <input type="checkbox" name="all-day" id={`${formId}-all-day`} />
+          <label htmlFor={`${formId}-all-day`}>All Day?</label>
         </div>
         <div className="row">
           <div className="form-group">
-            <label htmlFor="start-time">Start Time</label>
-            <input type="time" name="start-time" id="start-time" />
+            <label htmlFor={`${formId}-start-time`}>Start Time</label>
+            <input type="time" name="start-time" id={`${formId}-start-time`} />
           </div>
           <div className="form-group">
-            <label htmlFor="end-time">End Time</label>
-            <input type="time" name="end-time" id="end-time" />
+            <label htmlFor={`${formId}-end-time`}>End Time</label>
+            <input type="time" name="end-time" id={`${formId}-end-time`} />
           </div>
         </div>
         <div className="form-group">
           <label>Color</label>
           <div className="row left">
-            <input
-              type="radio"
-              name="color"
-              value="blue"
-              id="blue"
-              checked
-              className="color-radio"
-            />
-            <label htmlFor="blue">
-              <span className="sr-only">Blue</span>
-            </label>
-            <input
-              type="radio"
-              name="color"
-              value="red"
-              id="red"
-              className="color-radio"
-            />
-            <label htmlFor="red">
-              <span className="sr-only">Red</span>
-            </label>
-            <input
-              type="radio"
-              name="color"
-              value="green"
-              id="green"
-              className="color-radio"
-            />
-            <label htmlFor="green">
-              <span className="sr-only">Green</span>
-            </label>
+            {EVENT_COLORS.map((color) => (
+              <Fragment key={color}>
+                <input
+                  type="radio"
+                  name="color"
+                  value={color}
+                  id={`${formId}-${color}`}
+                  checked={selectedColor === color}
+                  onChange={() => setSelectedColor(color)}
+                  className="color-radio"
+                />
+                <label htmlFor={`${formId}-${color}`}>
+                  <span className="sr-only">{color}</span>
+                </label>
+              </Fragment>
+            ))}
           </div>
         </div>
         <div className="row">
           <button className="btn btn-success" type="submit">
-            Add
+            {isNew ? "Add" : "Edit"}
           </button>
-          <button className="btn btn-delete" type="button">
-            Delete
-          </button>
+          {onDelete != null && (
+            <button className="btn btn-delete" type="button" onClick={onDelete}>
+              Delete
+            </button>
+          )}
         </div>
       </form>
     </Modal>
--- src/context/useEvent.tsx
+++ src/context/useEvent.tsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { Context } from "./Events";
 
-export const EVENT_COLORS = ["red", "blue", "green"] as const;
+export const EVENT_COLORS = ["red", "green", "blue"] as const;
 
 export function useEvent() {
   const value = useContext(Context);
