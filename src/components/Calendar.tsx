9d5976a57f814a9a68245e8cc0c4eabb96f710ea
muuo <anthonymuuo92@gmail.com>
2025-06-08 13:01:17 +0300

    model opens well and closes well to edit the event

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -153,24 +153,36 @@ function CalendarDay({
 }
 
 function CalendarEvent({ event }: { event: Event }) {
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const { updateEvent, deleteEvent } = useEvent();
   return (
-    <button
-      className={cc("event", event.color, event.allDay && "all-day-event")}
-    >
-      {event.allDay ? (
-        <div className="event-name">{event.name}</div>
-      ) : (
-        <>
-          <div className={`color-dot ${event.color}`}></div>
-          <div className="event-time">
-            {formatDate(parse(event.startTime, "HH:mm", event.date), {
-              timeStyle: "short",
-            })}
-          </div>
+    <>
+      <button
+        onClick={() => setIsEditModalOpen(true)}
+        className={cc("event", event.color, event.allDay && "all-day-event")}
+      >
+        {event.allDay ? (
           <div className="event-name">{event.name}</div>
-        </>
-      )}
-    </button>
+        ) : (
+          <>
+            <div className={`color-dot ${event.color}`}></div>
+            <div className="event-time">
+              {formatDate(parse(event.startTime, "HH:mm", event.date), {
+                timeStyle: "short",
+              })}
+            </div>
+            <div className="event-name">{event.name}</div>
+          </>
+        )}
+      </button>
+      <EventFormModal
+        event={event}
+        isOpen={isEditModalOpen}
+        onClose={() => setIsEditModalOpen(false)}
+        onSubmit={(e) => updateEvent(event.id, e)}
+        onDelete={() => deleteEvent(event.id)}
+      />
+    </>
   );
 }
 
